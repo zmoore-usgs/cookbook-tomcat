1a226cc5014e97875f2806de2d34982a84bf322d
Suftin, Ivan <isuftin@usgs.gov>
2014-12-27 20:57:47 -0600

    Added ServerSpec + Bats IT
    Updated /opt/tomcat to belong to user tomcat, group root

---
+++ test/integration/default/serverspec/recipes_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+# https://github.com/rspec/rspec-expectations/blob/master/Should.md
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]  # default, enables both `should` and `expect`
+  end
+end
