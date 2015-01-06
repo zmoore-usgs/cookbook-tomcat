4bf82b1bf29c81acc52534184d90830dc5b8cc80
Suftin, Ivan <isuftin@usgs.gov>
2015-01-06 10:32:19 -0600

    Removed psiprobe specific deploy info

--- attributes/deploy_application.rb
+++
@@ -1,3 +0,0 @@
-# These attributes are used in the example cookbook for deploying applications
-default[:wsi_tomcat][:instances][:default][:application][:psiprobe][:url]                 = 'http://cida.usgs.gov/maven/cida-public-thirdparty/com/googlecode/psiprobe/web/2.3.3/web-2.3.3.war'
-default[:wsi_tomcat][:instances][:default][:application][:psiprobe][:final_name]          = 'psi-probe'
