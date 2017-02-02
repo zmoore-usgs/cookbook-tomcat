ad7b31ebd62b20ee3a0bd976298ac35209f8ec5e
Suftin, Ivan <isuftin@usgs.gov>
2017-02-02 11:32:53 -0600

    Got rid of some cruft in tests. moved Test Kitchen to use CentOS 6.8

--- test/integration/default/inspec/install_deps_spec.rb
+++ test/integration/default/inspec/install_deps_spec.rb
@@ -1,15 +1,4 @@
 
-
-# Check to make sure Java is installed
-describe command('echo $JAVA_HOME') do
-  its(:stdout) { should start_with('/usr/lib/jvm/java')}
-end
-
-# Where is it installed?
-describe command('which java') do
-  its('stdout') { should include '/usr/bin/java' }
-end
-
 # Check to make sure gcc is installed
 describe package('gcc') do
   it { should be_installed }
